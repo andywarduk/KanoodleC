637584b4d774ee012a74e182031a30630ab08bd4
Andrew Ward <ajw@gbh-warda-mac.local>
2013-07-17 17:26:13 +0100

    Initial Commit

---
+++ Kanoodle/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Kanoodle
+//
+//  Created by Andrew Ward on 17/07/2013.
+//  Copyright (c) 2013 Andrew Ward. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
