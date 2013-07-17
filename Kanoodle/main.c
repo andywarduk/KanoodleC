e3e3e904db48fde8f5a5ccff6abfdf38b44a361e
Andrew Ward <ajw@gbh-warda-mac.local>
2013-07-17 17:41:38 +0100

    Initial import

--- Kanoodle/main.c
+++ Kanoodle/main.c
@@ -1,18 +1,32 @@
 //
 //  main.c
-//  Kanoodle
+//  KanoodleC
 //
-//  Created by Andrew Ward on 17/07/2013.
+//  Created by Andrew Ward on 12/07/2013.
 //  Copyright (c) 2013 Andrew Ward. All rights reserved.
 //
 
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "Shapes.h"
+#include "Board.h"
+#include "SolutionMgr.h"
+#include "ShapeFit.h"
 
 int main(int argc, const char * argv[])
 {
+    struct SolutionMgr SolutionOutput;
 
-    // insert code here...
-    printf("Hello, World!\n");
+    InitShapes();
+    
+    CreateBoardShapes();
+    
+    InitSolutionMgr(&SolutionOutput);
+
+    FitShapes(&SolutionOutput);
+    
     return 0;
 }
 
